814f50e3e4fc1e6d4ee00fc07888a62effd1b86c
Sam Steel <samuel.ini145@gmail.com>
2024-05-14 17:15:54 +0100

    enterRaffle implement

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,16 @@ import React, { useState, useEffect } from 'react';
 import { useReadContract } from 'wagmi'
 import { useWeb3ModalProvider, useWeb3ModalAccount } from '@web3modal/ethers/react'
 import { BrowserProvider, Contract, formatUnits } from 'ethers'
+import { useWriteContract } from 'wagmi'
+import { ethers } from 'ethers';
+import { parseEther } from "viem";
+
 
 const RaffleAddress = '0xA2e48f1dB93592be6E28636E2D5254F705633c61'
 const RaffleAbi = [{"inputs":[{"internalType":"address","name":"vrfCordinatorV2","type":"address"},{"internalType":"uint256","name":"entranceFee","type":"uint256"},{"internalType":"bytes32","name":"gasLane","type":"bytes32"},{"internalType":"uint64","name":"subscriptionId","type":"uint64"},{"internalType":"uint256","name":"interval","type":"uint256"},{"internalType":"uint32","name":"callbackGasLimit","type":"uint32"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint256","name":"currentBalance","type":"uint256"},{"internalType":"uint256","name":"numPlayers","type":"uint256"},{"internalType":"uint256","name":"raffleState","type":"uint256"}],"name":"Lottery_UpkeepNotNeeded","type":"error"},{"inputs":[],"name":"Lottery__NotEnoughETHEntered","type":"error"},{"inputs":[],"name":"Lottery__NotOpen","type":"error"},{"inputs":[],"name":"Lottery__TransferFailed","type":"error"},{"inputs":[{"internalType":"address","name":"have","type":"address"},{"internalType":"address","name":"want","type":"address"}],"name":"OnlyCoordinatorCanFulfill","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"player","type":"address"}],"name":"LotteryEnter","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"participant","type":"address"}],"name":"RaffleEnter","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"requestId","type":"uint256"}],"name":"RequestedRaffleWinner","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"winner","type":"address"}],"name":"WinnerPicked","type":"event"},{"inputs":[{"internalType":"bytes","name":"","type":"bytes"}],"name":"checkUpkeep","outputs":[{"internalType":"bool","name":"upkeepNeeded","type":"bool"},{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"enterRaffle","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"getEntranceFee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getLatestTimeStamp","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getNumWords","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"getNumberOfplayers","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"getPlayer","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRaffleState","outputs":[{"internalType":"enum Raffle.RaffleState","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRecentWinner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRequestConfimations","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"bytes","name":"","type":"bytes"}],"name":"performUpkeep","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"requestId","type":"uint256"},{"internalType":"uint256[]","name":"randomWords","type":"uint256[]"}],"name":"rawFulfillRandomWords","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 
 export default function Home() {
+  const [transactionHash, setTransactionHash] = useState('');
   // const { address, chainId, isConnected } = useWeb3ModalAccount()
   // const { walletProvider } = useWeb3ModalProvider()
 
@@ -23,7 +28,26 @@ export default function Home() {
   //   console.log(formatUnits(PlayerNumber, 18))
   
   // }
-  
+  const { writeContractAsync, isPending } = useWriteContract();
+  const payableAmount = 0.01; // Amount in ETH
+
+  const handleRaffleEntryClick = async () => {
+   try {
+      const data = await writeContractAsync({
+        address: RaffleAddress,
+        abi: RaffleAbi,
+        functionName: 'enterRaffle',
+        value: parseEther("0.01"), 
+        args: [],
+        });
+        setTransactionHash(data)
+        window.alert('Transaction successful! Raffle entry submitted.');
+        console.log('Transaction successful! Raffle entry submitted: hash', data)
+   } catch(err) {
+      console.error(err);
+   } 
+}
+
 
   const result = useReadContract({
     abi: RaffleAbi,
@@ -58,7 +82,8 @@ export default function Home() {
  
 </div>
  <h1>The previous winner is {lastWinner?.toString()}</h1>
-  <button class="font-bold bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">Join Lottery</button>
+ {transactionHash && <div>Transaction Hash: {transactionHash}</div>}
+  <button class="font-bold bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"   disabled={isPending} onClick={handleRaffleEntryClick}>Join Lottery</button>
  <h2>Number of participants: {numberOfPlayers?.toString()} </h2>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
